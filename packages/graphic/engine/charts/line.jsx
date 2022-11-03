17a7a3fd36356b96cce88742f6e406e1ba6d7784
hale <hale@bytedance.com>
2022-11-03 21:12:14 +0800

    chore: save

--- packages/graphic/engine/charts/line.jsx
+++ packages/graphic/engine/charts/line.jsx
@@ -80,6 +80,7 @@ export default class Line extends Base {
     // 算法： https://www.jointjs.com/blog/how-to-create-nice-looking-curves-in-svg-with-fixed-tangents
     // https://www.researchgate.net/publication/41391547_Modeling_Shapes_for_Pattern_Recognition_A_Simple_Low-Cost_Spline-based_Approach
     // https://github.com/gitcnd/jspline
+    // https://pomax.github.io/bezierinfo/#projections
     getCurvePathData(points) {
         const positions = this.layoutPoints(points);
 
